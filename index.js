a852ad943f3019f571ff446da1e3a4b54cb8892a
Mitchelle-git <mitchmarios2@gmail.com>
2025-12-18 15:58:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -2,31 +2,35 @@
 
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
+  // Implement the function to change background color
   const r = Math.floor(Math.random() * 256)
   const g = Math.floor(Math.random() * 256)
   const b = Math.floor(Math.random() * 256)
   document.body.style.setProperty(
     'background-color',
-    `rgb(${r}, ${g}, ${b})`) // Implement the function to change background color
-}
+    `rgb(${r}, ${g}, ${b})`)
+  }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
+  // Implement the function to reset background color
+
  document.body.style.backgroundColor = ''
-}  // Implement the function to reset background color
+}  
 
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
+  // Implement the function to display key pressed
     const keyDisplay = document.getElementById('keyPressDisplay')
   if (keyDisplay) {
     keyDisplay.textContent = `Key pressed: ${event.key}`
   }
   }
 
-  // Implement the function to display key pressed
+  
 
 
 // Process Text Input
